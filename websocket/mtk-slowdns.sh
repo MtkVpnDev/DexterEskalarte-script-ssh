9294e3439f382840d16a841b28301bf9b0cfbff6
MtkVpnDev <129391971+MtkVpnDev@users.noreply.github.com>
2023-04-28 10:14:46 +0800

    Update mtk-slowdns.sh

--- websocket/mtk-slowdns.sh
+++ websocket/mtk-slowdns.sh
@@ -60,8 +60,8 @@ useradd -p $(openssl passwd -1 12345) bulala -ou 0 -g 0
 sleep 2
 export EDITOR=nano
 sleep 2
-crontab -r -u kutsaratinidor
-(crontab -l 2>/dev/null || true; echo "@reboot screen -dmS slowdns /etc/ppp/dnstt/dnstt-server/./dnstt-server -udp :53 -privkey-file /etc/ppp/server.key $domain 127.0.0.1:22") | crontab - -u kutsaratinidor
+crontab -r -u bulala
+(crontab -l 2>/dev/null || true; echo "@reboot screen -dmS slowdns /etc/ppp/dnstt/dnstt-server/./dnstt-server -udp :53 -privkey-file /etc/ppp/server.key $domain 127.0.0.1:22") | crontab - -u bulala
 sleep 2
 /etc/init.d/cron restart
 clear
