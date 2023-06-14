cd26cbccfcd6c4a986807bb4d0d04d5bcb6f7907
MtkVpnDev <129391971+MtkVpnDev@users.noreply.github.com>
2023-06-14 16:39:19 +0800

    Update Debian10VPS-Installer-Slowds.sh

--- websocket/Debian10VPS-Installer-Slowds.sh
+++ websocket/Debian10VPS-Installer-Slowds.sh
@@ -779,11 +779,6 @@ sed -i '/#net.ipv4.ip_forward.*/d' /etc/sysctl.d/*
 echo 'net.ipv4.ip_forward=1' > /etc/sysctl.d/20-openvpn.conf
 sysctl --system &> /dev/null
 
-if [[ "$(hostnamectl | grep -i Virtualization | awk '{print $2}' | head -n1)" == 'openvz' ]]; then
- sed -i 's|LimitNPROC|#LimitNPROC|g' /lib/systemd/system/openvpn*
- systemctl daemon-reload
-fi
-
 sed -i 's|ExecStart=.*|ExecStart=/usr/sbin/openvpn --status %t/openvpn-server/status-%i.log --status-version 2 --suppress-timestamps --config %i.conf|g' /lib/systemd/system/openvpn-server\@.service
 systemctl daemon-reload
 
